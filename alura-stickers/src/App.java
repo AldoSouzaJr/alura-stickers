944edd96479f4c24981f726d2974dbaafafbc467
AldoSouzaJr <as.souza.jr@gmail.com>
2022-07-19 22:47:11 +0200

    Aula 1 da Imersao Java da Alura

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -11,6 +11,7 @@ public class App {
 	public static void main(String[] args) throws Exception {
 
 		// fazer uma conexão HTTP e buscar os top 250 filmes
+		
 		String url = "https://imdb-api.com/en/API/Top250Movies/k_9pgylnim";
 		URI endereco = URI.create(url);
 		var client = HttpClient.newHttpClient();
@@ -19,10 +20,12 @@ public class App {
 		String body = response.body();
 
 		// extrair só os dados que interessam (titulo, poster, classificação)
+		
 		var parser = new JsonParser();
 		List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
 		// exibir e manipular os dados
+		
 		for (Map<String, String> filme : listaDeFilmes) {
 			System.out.println(filme.get("title"));
 			System.out.println(filme.get("image"));
